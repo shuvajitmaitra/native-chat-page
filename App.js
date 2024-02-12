4c3586620225f1ab5cb0b55cd1838026fb979a1a
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2024-02-12 08:02:05 +0600

    Initital Project Setup

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Chat from "./App/Pages/Chat";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./App/Pages/Home";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Home"
+          component={Home}
+        />
+        <Stack.Screen name="Chat" component={Chat} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ App/Pages/Home.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Chat from "./Chat";
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Chat />
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+});
