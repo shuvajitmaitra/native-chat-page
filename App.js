e3211b169fcc7667ba29cf7507d5bb5cd4dad050
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2024-02-14 08:19:09 +0600

    Thread component create

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Home from "./App/Pages/Home";
 import TotalChats from "./App/Pages/TotalChats";
+import Thread from "./App/Pages/Thread";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
@@ -17,6 +18,11 @@ export default function App() {
           name="TotalChats"
           options={{ headerTitle: "Chat" }}
           component={TotalChats}
+        />
+        <Stack.Screen
+          name="Thread"
+          options={{ headerTitle: "Thread" }}
+          component={Thread}
         />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ App/Pages/Thread.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Thread = () => {
+  return (
+    <View>
+      <Text>Thread</Text>
+    </View>
+  );
+};
+
+export default Thread;
+
+const styles = StyleSheet.create({});
