ea9e471adb2ecd26e637a4837603c10c9f860363
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2024-02-20 13:29:18 +0600

    three dot pop up showed and styling done

--- App/Component/GroupProfileView/GroupMemberInfo.js
+++ App/Component/GroupProfileView/GroupMemberInfo.js
@@ -1,42 +1,131 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { responsiveScreenFontSize, responsiveScreenHeight, responsiveScreenWidth } from 'react-native-responsive-dimensions'
-import ThreeDotGrayIcon from '../../../assets/svgs/ThreeDotGrayIcon'
-import Fonts from '../../../assets/Fonts/Fonts'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import {
+  responsiveScreenFontSize,
+  responsiveScreenHeight,
+  responsiveScreenWidth,
+} from "react-native-responsive-dimensions";
+import { Popover, PopoverController } from "react-native-modal-popover";
 
-const GroupMemberInfo = ({item, index}) => {
+import ThreeDotGrayIcon from "../../../assets/svgs/ThreeDotGrayIcon";
+import Fonts from "../../../assets/Fonts/Fonts";
+import VolumeMute from "../../../assets/svgs/VolumeMute";
+import AddUsers from "../../../assets/svgs/AddUser";
+import BlockIcon from "../../../assets/svgs/BlockIcon";
+import BinIcon from "../../../assets/svgs/BinIcon";
+
+const GroupMemberInfo = ({ item, index }) => {
   return (
-
-      <View style={[styles.container, {  borderTopColor: index ? "rgba(0, 0, 0, 0.1)": "white"}]}>
-        {/* -------------------------- */}
-        {/* ----------- Profile Image ----------- */} 
-        {/* -------------------------- */}
-        <View style={styles.profileImageContainer}>
-          <View>
-            <Image style={styles.profileImage} source={item.profileImage} />
-          </View>
-          <View>
-            <Text style={styles.profileName}>{item.userName} {item.role==="Member" || <Text style={styles.roleText}>{`(${item.role})`}</Text>}</Text>
-            <Text style={[styles.status, {color: item.status ==="Online" ? "#06AC6D" : "#666666"} ]}>{item.status}</Text>
-          </View>
-        </View>
-        {/* -------------------------- */}
-        {/* ----------- Comment time ----------- */}
-        {/* -------------------------- */}
+    <View
+      style={[
+        styles.container,
+        { borderTopColor: index ? "rgba(0, 0, 0, 0.1)" : "white" },
+      ]}
+    >
+      {/* -------------------------- */}
+      {/* ----------- Profile Image ----------- */}
+      {/* -------------------------- */}
+      <View style={styles.profileImageContainer}>
         <View>
-      <ThreeDotGrayIcon/>
+          <Image style={styles.profileImage} source={item.profileImage} />
+        </View>
+        <View>
+          <Text style={styles.profileName}>
+            {item.userName}{" "}
+            {item.role === "Member" || (
+              <Text style={styles.roleText}>{`(${item.role})`}</Text>
+            )}
+          </Text>
+          <Text
+            style={[
+              styles.status,
+              { color: item.status === "Online" ? "#06AC6D" : "#666666" },
+            ]}
+          >
+            {item.status}
+          </Text>
         </View>
       </View>
-  )
-}
+      {/* -------------------------- */}
+      {/* ----------- Three dot icon ----------- */}
+      {/* -------------------------- */}
+      <PopoverController>
+        {({
+          openPopover,
+          closePopover,
+          popoverVisible,
+          setPopoverAnchor,
+          popoverAnchorRect,
+        }) => (
+          <React.Fragment>
+            <TouchableOpacity
+              style={styles.threeDotIcon}
+              ref={setPopoverAnchor}
+              onPress={openPopover}
+            >
+              <ThreeDotGrayIcon />
+            </TouchableOpacity>
+            <Popover
+              contentStyle={styles.content}
+              arrowStyle={styles.arrow}
+              backgroundStyle={styles.background}
+              visible={popoverVisible}
+              onClose={closePopover}
+              fromRect={popoverAnchorRect}
+              placement="bottom"
+              supportedOrientations={["portrait", "landscape"]}
+            >
+              <View>
+                <View style={styles.buttonContainer}>
+                  <TouchableOpacity>
+                    <View style={styles.iconAndTextContainer}>
+                      <AddUsers />
+                      <Text style={styles.buttonText}>Role</Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+                <View style={styles.buttonContainer}>
+                  <TouchableOpacity>
+                    <View style={styles.iconAndTextContainer}>
+                      <VolumeMute />
+                      <Text style={styles.buttonText}>Mute</Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+                <View style={styles.buttonContainer}>
+                  <TouchableOpacity>
+                    <View style={styles.iconAndTextContainer}>
+                      <BlockIcon />
+                      <Text style={styles.buttonText}>Block</Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+                <View
+                  style={[styles.buttonContainer, { borderBottomWidth: 0 }]}
+                >
+                  <TouchableOpacity>
+                    <View style={styles.iconAndTextContainer}>
+                      <BinIcon />
+                      <Text style={styles.buttonText}>Remove</Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </Popover>
+          </React.Fragment>
+        )}
+      </PopoverController>
+    </View>
+  );
+};
 
-export default GroupMemberInfo
+export default GroupMemberInfo;
 
 const styles = StyleSheet.create({
-  roleText:{
+  roleText: {
     color: "rgba(0, 0, 0, 0.5)",
     fontFamily: "Inter-Regular",
-    fontSize: responsiveScreenFontSize(1.5)
+    fontSize: responsiveScreenFontSize(1.5),
   },
   container: {
     flexDirection: "row",
@@ -63,7 +152,7 @@ const styles = StyleSheet.create({
 
     fontWeight: "500",
     color: "black",
-    fontFamily: "Inter-Medium"
+    fontFamily: "Inter-Medium",
   },
   status: {
     color: "rgba(11, 42, 70, 1)",
@@ -73,5 +162,31 @@ const styles = StyleSheet.create({
   commentsTime: {
     color: "rgba(111, 116, 124, 1)",
     fontSize: responsiveScreenFontSize(1.9),
+  },
+  // --------------------------
+  // ----------- Popup modal -----------
+  // --------------------------
+  threeDotIcon: {
+    paddingHorizontal: responsiveScreenWidth(1),
+  },
+  buttonText: {
+    fontSize: responsiveScreenFontSize(2),
+    color: "#666",
+  },
+  iconAndTextContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: responsiveScreenWidth(3),
+  },
+  buttonContainer: {
+    paddingVertical: responsiveScreenHeight(1.5),
+    paddingHorizontal: responsiveScreenWidth(1.5),
+    borderBottomWidth: 1,
+    borderBottomColor: "rgba(0, 0, 0, 0.1)",
+  },
+
+  content: {
+    borderRadius: 8,
+    minWidth: responsiveScreenWidth(50),
   },
 });
--- assets/svgs/BlockIcon.js
+++ assets/svgs/BlockIcon.js
@@ -1,36 +1,58 @@
 import * as React from "react";
-import Svg, { Path, Text } from "react-native-svg";
+import Svg, { G, Path, Rect } from "react-native-svg";
 
 function SvgComponent(props) {
   return (
     <Svg
       xmlns="http://www.w3.org/2000/svg"
-      x="0px"
-      y="0px"
-      viewBox="0 0 100 125"
+      width={19}
+      height={19}
+      viewBox="0 0 256 256"
       {...props}
     >
-      <Path d="M49.438 84.75c-19.196 0-34.813-15.617-34.813-34.813s15.617-34.813 34.813-34.813S84.25 30.742 84.25 49.938 68.633 84.75 49.438 84.75zm0-66.625c-17.542 0-31.813 14.271-31.813 31.813S31.896 81.75 49.438 81.75 81.25 67.479 81.25 49.938 66.979 18.125 49.438 18.125z" />
-      <Path
-        transform="rotate(40.1 49.542 50.03)"
-        d="M48.039 16.713H51.039V83.33600000000001H48.039z"
-      />
-      <Text
-        y={115}
-        fontSize="5px"
-        fontWeight="bold"
-        fontFamily="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif"
+      <G
+        transform="matrix(2.81 0 0 2.81 1.407 1.407)"
+        stroke="none"
+        strokeWidth={0}
+        strokeDasharray="none"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeMiterlimit={10}
+        fill="none"
+        fillRule="nonzero"
+        opacity={1}
       >
-        {"Created by SANB"}
-      </Text>
-      <Text
-        y={120}
-        fontSize="5px"
-        fontWeight="bold"
-        fontFamily="'Helvetica Neue', Helvetica, Arial-Unicode, Arial, Sans-serif"
-      >
-        {"from the Noun Project"}
-      </Text>
+        <Path
+          d="M45 90c-12.02 0-23.32-4.681-31.82-13.181C4.681 68.32 0 57.02 0 45s4.681-23.32 13.18-31.82C21.68 4.681 32.98 0 45 0s23.32 4.681 31.819 13.18C85.319 21.68 90 32.98 90 45s-4.681 23.32-13.181 31.819C68.32 85.319 57.02 90 45 90zm0-82c-9.883 0-19.174 3.849-26.163 10.837C11.849 25.826 8 35.117 8 45s3.849 19.174 10.837 26.163C25.826 78.151 35.117 82 45 82s19.174-3.849 26.163-10.837C78.151 64.174 82 54.883 82 45s-3.849-19.174-10.837-26.163C64.174 11.849 54.883 8 45 8z"
+          stroke="none"
+          strokeWidth={1}
+          strokeDasharray="none"
+          strokeLinecap="butt"
+          strokeLinejoin="miter"
+          strokeMiterlimit={10}
+          fill="rgba(99, 99, 99, 1)"
+          fillRule="nonzero"
+          opacity={1}
+        />
+        <Rect
+          x={4}
+          y={41}
+          rx={0}
+          ry={0}
+          width={82}
+          height={8}
+          transform="rotate(-45.009 44.995 44.999)"
+          stroke="none"
+          strokeWidth={1}
+          strokeDasharray="none"
+          strokeLinecap="butt"
+          strokeLinejoin="miter"
+          strokeMiterlimit={10}
+          fill="rgba(99, 99, 99, 1)"
+          fillRule="nonzero"
+          opacity={1}
+        />
+      </G>
     </Svg>
   );
 }
