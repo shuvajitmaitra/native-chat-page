270768ac938ce5847fb05555eb1bf8a17ed8d7aa
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2024-03-03 15:12:10 +0600

    calendar item showed

--- App/Pages/Calendar/Calendar.js
+++ App/Pages/Calendar/Calendar.js
@@ -5,6 +5,8 @@ import {
   responsiveScreenHeight,
   responsiveScreenWidth,
 } from "react-native-responsive-dimensions";
+import RightArrowLong from "../../../assets/svgs/RightArrowLong";
+import Fonts from "../../../assets/Fonts/Fonts";
 
 const Calendar = () => {
   return (
@@ -13,6 +15,38 @@ const Calendar = () => {
         <Text style={styles.weekText}>Week 1</Text>
         <Text style={styles.weekText}>Jan 1 - Jan 7</Text>
       </View>
+      <View style={styles.eventTopicContainer}>
+        <View style={styles.eventDateContainer}>
+          <View style={styles.eventDate}>
+            <Text style={styles.eventDay}>Mon</Text>
+            <Text style={styles.eventDateNumber}>6</Text>
+          </View>
+          <RightArrowLong />
+        </View>
+        <View>
+          <View style={styles.eventTitleAndDateContainer}>
+            <Text>Show N Tell(Finished)</Text>
+            <Text>10PM - 11 PM</Text>
+          </View>
+          <Text>Edited: Abdullah Noman - Feb 20, 2024</Text>
+        </View>
+      </View>
+      <View style={styles.weekContainer}>
+        <Text style={styles.weekText}>Week 2</Text>
+        <Text style={styles.weekText}>Jan 8 - Jan 14</Text>
+      </View>
+      <View style={styles.weekContainer}>
+        <Text style={styles.weekText}>Week 3</Text>
+        <Text style={styles.weekText}>Jan 15 - Jan 21</Text>
+      </View>
+      <View style={styles.weekContainer}>
+        <Text style={styles.weekText}>Week 4</Text>
+        <Text style={styles.weekText}>Jan 22 - Jan 28</Text>
+      </View>
+      <View style={styles.weekContainer}>
+        <Text style={styles.weekText}>Week 5</Text>
+        <Text style={styles.weekText}>Jan 29 - Jan 24</Text>
+      </View>
     </View>
   );
 };
@@ -20,16 +54,39 @@ const Calendar = () => {
 export default Calendar;
 
 const styles = StyleSheet.create({
+  eventTitleAndDateContainer: {
+    backgroundColor: "#619DCC",
+    padding: 10,
+    borderRadius: 10,
+    width: "100%",
+  },
+  eventDateNumber: {
+    fontFamily: "Inter-Regular",
+    fontSize: responsiveScreenFontSize(3),
+  },
+  eventDateContainer: {
+    backgroundColor: "red",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: responsiveScreenWidth(2),
+  },
+  eventTopicContainer: {
+    backgroundColor: "green",
+    flexDirection: "row",
+    gap: responsiveScreenWidth(2),
+  },
   weekText: {
     color: "#546A7E",
     fontSize: responsiveScreenFontSize(1.4),
   },
   weekContainer: {
     flexDirection: "row",
+
     gap: responsiveScreenWidth(5),
   },
   calenderTopicContainer: {
     paddingVertical: responsiveScreenHeight(2),
     paddingHorizontal: responsiveScreenWidth(4),
+    gap: responsiveScreenHeight(1.5),
   },
 });
---
+++ assets/svgs/RightArrowLong.js
@@ -0,0 +1,22 @@
+import * as React from "react";
+import Svg, { Path } from "react-native-svg";
+
+function RightArrowLong(props) {
+  return (
+    <Svg
+      width={25}
+      height={20}
+      viewBox="0 0 22 8"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <Path
+        d="M21.354 4.354a.5.5 0 000-.708L18.172.464a.5.5 0 10-.707.708L20.293 4l-2.828 2.828a.5.5 0 10.707.708l3.182-3.182zM0 4.5h21v-1H0v1z"
+        fill="#0B2A46"
+      />
+    </Svg>
+  );
+}
+
+export default RightArrowLong;
