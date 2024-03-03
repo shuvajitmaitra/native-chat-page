8897d7ef4ab786821ab2fef0dcc855e1f4072ae5
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2024-03-03 14:17:08 +0600

    week showed

--- App/Pages/Calendar/Calendar.js
+++ App/Pages/Calendar/Calendar.js
@@ -1,14 +1,35 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import {
+  responsiveScreenFontSize,
+  responsiveScreenHeight,
+  responsiveScreenWidth,
+} from "react-native-responsive-dimensions";
 
 const Calendar = () => {
   return (
-    <View>
-      <Text>Calendar</Text>
+    <View style={styles.calenderTopicContainer}>
+      <View style={styles.weekContainer}>
+        <Text style={styles.weekText}>Week 1</Text>
+        <Text style={styles.weekText}>Jan 1 - Jan 7</Text>
+      </View>
     </View>
   );
 };
 
 export default Calendar;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  weekText: {
+    color: "#546A7E",
+    fontSize: responsiveScreenFontSize(1.4),
+  },
+  weekContainer: {
+    flexDirection: "row",
+    gap: responsiveScreenWidth(5),
+  },
+  calenderTopicContainer: {
+    paddingVertical: responsiveScreenHeight(2),
+    paddingHorizontal: responsiveScreenWidth(4),
+  },
+});
