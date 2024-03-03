2c87ce69b2f9732bf48d05936ba7fb90ffbbf700
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2024-03-03 17:27:26 +0600

    data showed dynamically.

--- App/Pages/Calendar/Calendar.js
+++ App/Pages/Calendar/Calendar.js
@@ -1,7 +1,6 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import {
-  responsiveFontSize,
   responsiveScreenFontSize,
   responsiveScreenHeight,
   responsiveScreenWidth,
@@ -9,106 +8,150 @@ import {
 import RightArrowLong from "../../../assets/svgs/RightArrowLong";
 import Fonts from "../../../assets/Fonts/Fonts";
 
+// --------------------------
+// ----------- Calendar Item Components -----------
+// --------------------------
+
+export const CalendarItems = (item) => {
+  return (
+    <View style={styles.eventTopicContainer}>
+      <View style={styles.eventDateContainer}>
+        <View style={styles.eventDate}>
+          <Text style={styles.eventDay}>{item.item.day}</Text>
+          <Text style={styles.eventDateNumber}>{item.item.date}</Text>
+        </View>
+        <RightArrowLong />
+      </View>
+      <View
+        style={{
+          // backgroundColor: "salmon",
+          width: responsiveScreenWidth(75),
+        }}
+      >
+        <View
+          style={[
+            styles.eventTitleAndDateContainer,
+            {
+              backgroundColor:
+                (item.item.status === "proposed new time" && "#619DCC") ||
+                (item.item.status === "accepted" && "#27AC1F") ||
+                (item.item.status === "pending" && "#FF9900") ||
+                (item.item.status === "denied" && "#00C177") ||
+                (item.item.status === "rejected" && "#EF4E52") ||
+                (item.item.status === "finished" && "#7666FF"),
+            },
+          ]}
+        >
+          <Text style={{ color: "white", fontFamily: "WorkSans-Regular" }}>
+            {item.item.title}
+          </Text>
+          <Text
+            style={{
+              color: "white",
+              fontFamily: "WorkSans-Regular",
+              fontSize: responsiveScreenFontSize(1.5),
+            }}
+          >
+            {item.item.time}
+          </Text>
+        </View>
+        <Text style={styles.EditorNameDate}>
+          <Text style={{ color: "black", fontWeight: "500" }}>Edited:</Text>{" "}
+          Abdullah Noman - Feb 20, 2024
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+// --------------------------
+// ----------- Main Components -----------
+// --------------------------
+
 const Calendar = () => {
   const data = [
     {
       title: "Show N Tell (Finished)",
-      Time: "10PM - 11PM",
+      time: "10PM - 11PM",
       day: "Mon",
       date: 6,
+      status: "proposed new time",
     },
     {
       title: "Show N Tell (Finished)",
-      Time: "10PM - 11PM",
+      time: "10PM - 11PM",
       day: "Mon",
-      date: 6,
+      date: 9,
+      status: "accepted",
     },
     {
       title: "Show N Tell (Finished)",
-      Time: "10PM - 11PM",
+      time: "10PM - 11PM",
       day: "Mon",
-      date: 6,
+      date: 12,
+      status: "pending",
     },
     {
       title: "Show N Tell (Finished)",
-      Time: "10PM - 11PM",
+      time: "10PM - 11PM",
       day: "Mon",
-      date: 6,
+      date: 14,
+      status: "denied",
     },
     {
       title: "Show N Tell (Finished)",
-      Time: "10PM - 11PM",
+      time: "10PM - 11PM",
       day: "Mon",
-      date: 6,
-    },
-    {
-      title: "Show N Tell (Finished)",
-      Time: "10PM - 11PM",
-      day: "Mon",
-      date: 6,
+      date: 25,
+      status: "proposed new time",
     },
   ];
   return (
-    <View style={styles.calenderTopicContainer}>
-      <View style={styles.weekContainer}>
-        <Text style={styles.weekText}>Week 1</Text>
-        <Text style={styles.weekText}>Jan 1 - Jan 7</Text>
-      </View>
-      {/* -------------------------- */}
-      {/* ----------- Container ----------- */}
-      {/* -------------------------- */}
-      <View style={styles.eventTopicContainer}>
-        <View style={styles.eventDateContainer}>
-          <View style={styles.eventDate}>
-            <Text style={styles.eventDay}>Mon</Text>
-            <Text style={styles.eventDateNumber}>6</Text>
-          </View>
-          <RightArrowLong />
+    <ScrollView>
+      <View style={styles.calenderTopicContainer}>
+        <View style={styles.weekContainer}>
+          <Text style={styles.weekText}>Week 1</Text>
+          <Text style={styles.weekText}>Jan 1 - Jan 7</Text>
         </View>
-        <View
-          style={{
-            // backgroundColor: "salmon",
-            width: responsiveScreenWidth(75),
-          }}
-        >
-          <View style={styles.eventTitleAndDateContainer}>
-            <Text style={{ color: "white", fontFamily: "WorkSans-Regular" }}>
-              Show N Tell(Finished)
-            </Text>
-            <Text
-              style={{
-                color: "white",
-                fontFamily: "WorkSans-Regular",
-                fontSize: responsiveScreenFontSize(1.5),
-              }}
-            >
-              10PM - 11 PM
-            </Text>
-          </View>
-          <Text style={styles.EditorNameDate}>
-            <Text style={{ color: "black", fontWeight: "500" }}>Edited:</Text>{" "}
-            Abdullah Noman - Feb 20, 2024
-          </Text>
-        </View>
-      </View>
+        {data.map(
+          (item, index) =>
+            item.date >= 1 && item.date <= 7 && <CalendarItems item={item} />
+        )}
 
-      <View style={styles.weekContainer}>
-        <Text style={styles.weekText}>Week 2</Text>
-        <Text style={styles.weekText}>Jan 8 - Jan 14</Text>
+        <View style={styles.weekContainer}>
+          <Text style={styles.weekText}>Week 2</Text>
+          <Text style={styles.weekText}>Jan 8 - Jan 14</Text>
+        </View>
+        {data.map(
+          (item, index) =>
+            item.date >= 8 && item.date <= 14 && <CalendarItems item={item} />
+        )}
+        <View style={styles.weekContainer}>
+          <Text style={styles.weekText}>Week 3</Text>
+          <Text style={styles.weekText}>Jan 15 - Jan 21</Text>
+        </View>
+        {data.map(
+          (item, index) =>
+            item.date >= 15 && item.date <= 21 && <CalendarItems item={item} />
+        )}
+        <View style={styles.weekContainer}>
+          <Text style={styles.weekText}>Week 4</Text>
+          <Text style={styles.weekText}>Jan 22 - Jan 28</Text>
+        </View>
+        {data.map(
+          (item, index) =>
+            item.date >= 22 && item.date <= 28 && <CalendarItems item={item} />
+        )}
+        <View style={styles.weekContainer}>
+          <Text style={styles.weekText}>Week 5</Text>
+          <Text style={styles.weekText}>Jan 29 - Feb 4</Text>
+        </View>
+        {data.map(
+          (item, index) =>
+            item.date >= 29 && item.date <= 31 && <CalendarItems item={item} />
+        )}
       </View>
-      <View style={styles.weekContainer}>
-        <Text style={styles.weekText}>Week 3</Text>
-        <Text style={styles.weekText}>Jan 15 - Jan 21</Text>
-      </View>
-      <View style={styles.weekContainer}>
-        <Text style={styles.weekText}>Week 4</Text>
-        <Text style={styles.weekText}>Jan 22 - Jan 28</Text>
-      </View>
-      <View style={styles.weekContainer}>
-        <Text style={styles.weekText}>Week 5</Text>
-        <Text style={styles.weekText}>Jan 29 - Jan 24</Text>
-      </View>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -125,7 +168,6 @@ const styles = StyleSheet.create({
     width: "100%",
   },
   eventTitleAndDateContainer: {
-    backgroundColor: "#619DCC",
     paddingHorizontal: responsiveScreenWidth(2),
     paddingVertical: responsiveScreenHeight(0.5),
     borderRadius: 10,
@@ -153,12 +195,13 @@ const styles = StyleSheet.create({
   },
   weekContainer: {
     flexDirection: "row",
-    gap: responsiveScreenWidth(5),
+    gap: responsiveScreenWidth(6.5),
   },
   calenderTopicContainer: {
     paddingVertical: responsiveScreenHeight(2),
     paddingHorizontal: responsiveScreenWidth(4),
     gap: responsiveScreenHeight(1.5),
     // width: "100%",
+    backgroundColor: "white",
   },
 });
